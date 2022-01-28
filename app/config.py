3d4acae3ec05ac92337d2385261d53f5e3480b48
jimmy-oss <jimmynjonge51@gmail.com>
2022-01-28 10:05:39 +0300

    macros solved files

---
+++ app/config.py
@@ -0,0 +1,28 @@
+class Config:
+    '''
+    General configuration parent class
+    '''
+    MOVIE_API_BASE_URL ='https://api.themoviedb.org/3/movie/{}?api_key={}'
+    MOVIE_API_KEY ='922754fd6200c973b3d59e0e6d65d3aa'
+
+
+
+class ProdConfig(Config):
+    '''
+    Production  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    pass
+
+
+class DevConfig(Config):
+    '''
+    Development  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+
+    DEBUG = True
---
+++ app/views.py
@@ -0,0 +1,28 @@
+from flask import render_template
+from app import app
+from .request import get_movies
+import ssl
+
+
+ssl._create_default_https_context = ssl._create_unverified_context
+@app.route('/')
+def index():
+
+    '''
+    View root page function that returns the index page and its data
+    '''
+
+    # Getting popular movie
+    popular_movies = get_movies('popular')
+    print(popular_movies)
+    title = 'Home - Welcome to The best Movie Review Website Online'
+    return render_template('index.html', title = title,popular = popular_movies)
+    
+@app.route('/movie/<movie_id>')
+def movie(movie_id):
+
+    '''
+    View movie page function that returns the movie details page and its data
+    '''
+    title = f'You are viewing {movie_id}'
+    return render_template('movie.html',title = title)
