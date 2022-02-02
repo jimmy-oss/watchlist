d21434acfe264b42ff5483853872671516407b62
jimmy-oss <jimmynjonge51@gmail.com>
2022-02-02 16:51:46 +0300

    database authetication

--- app/__init__.py
+++ app/__init__.py
@@ -10,6 +10,9 @@ db = SQLAlchemy()
 def create_app(config_name):
 
     app = Flask(__name__)
+    
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
 
     # Creating the app configurations
 
