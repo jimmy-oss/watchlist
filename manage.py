0801e743e35198d7c376915fa5eca7fb7f1c34f0
jimmy-oss <jimmynjonge51@gmail.com>
2022-02-02 14:00:42 +0300

    configuring database error

--- manage.py
+++ manage.py
@@ -1,5 +1,6 @@
-from app import create_app
+from app import create_app,db
 from flask_script import Manager,Server
+from app.models import User
 
 # Creating app instance
 app = create_app('development')
@@ -7,6 +8,7 @@ app = create_app('development')
 manager = Manager(app)
 
 manager.add_command('server',Server)
+
 @manager.command
 def test():
     """Run the unit tests."""
@@ -14,7 +16,9 @@ def test():
     tests = unittest.TestLoader().discover('tests')
     unittest.TextTestRunner(verbosity=2).run(tests)
 
-
+@manager.shell
+def make_shell_context():
+    return dict(app = app,db = db,User = User )
 
 if __name__ == '__main__':
     manager.run()
