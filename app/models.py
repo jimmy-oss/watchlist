cf2117c8fed9ba68919384ed5010f1e68ae83964
jimmy-oss <jimmynjonge51@gmail.com>
2022-02-03 01:42:16 +0300

    deployment to heroku

--- app/models.py
+++ app/models.py
@@ -1,11 +1,10 @@
-from . import db
 class Movie:
     '''
     Movie class to define Movie Objects
     '''
 
-    def __init__(self,id,title,overview,poster,vote_average,vote_count):
-        self.id =id
+    def __init__(self, id, title, overview, poster, vote_average, vote_count):
+        self.id = id
         self.title = title
         self.overview = overview
         self.poster = "https://image.tmdb.org/t/p/w500/" + poster
@@ -13,28 +12,25 @@ class Movie:
         self.vote_count = vote_count
 
 
-
 class Review:
 
     all_reviews = []
 
-    def __init__(self,movie_id,title,imageurl,review):
+    def __init__(self, movie_id, title, imageurl, review):
         self.movie_id = movie_id
         self.title = title
         self.imageurl = imageurl
         self.review = review
 
-
     def save_review(self):
         Review.all_reviews.append(self)
-
 
     @classmethod
     def clear_reviews(cls):
         Review.all_reviews.clear()
 
     @classmethod
-    def get_reviews(cls,id):
+    def get_reviews(cls, id):
 
         response = []
 
@@ -43,26 +39,3 @@ class Review:
                 response.append(review)
 
         return response
-
-class User(db.Model):
-    __tablename__ = 'users'
-
-    id = db.Column(db.Integer,primary_key = True)
-    username = db.Column(db.String(255))
-    role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
-
-
-    def __repr__(self):
-        return f'User {self.username}'
-
-
-class Role(db.Model):
-    __tablename__ = 'roles'
-
-    id = db.Column(db.Integer,primary_key = True)
-    name = db.Column(db.String(255))
-    users = db.relationship('User',backref = 'role',lazy="dynamic")
-
-
-    def __repr__(self):
-        return f'User {self.name}'
